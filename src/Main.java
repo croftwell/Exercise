2fc6a4fa9ef4f24859db6a2fbcfa223db7b311a1
Ali Çavuşlu <cavuslumehmet@icloud.com>
2023-01-08 18:49:30 +0300

    .

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    hesapMakinesi makine1;
+    makine1 = new hesapMakinesi(1, 3);
+
+
+
+
+    }
+}
---
+++ src/hesapMakinesi.java
@@ -0,0 +1,24 @@
+public class hesapMakinesi {
+    public int sayi1;
+    public int sayi2;
+
+    hesapMakinesi(int sayi1, int sayi2) {
+
+        this.sayi1 = sayi1;
+        this.sayi2 = sayi2;
+
+    }
+
+    public int toplama() {
+        return this.sayi1 + this.sayi2;
+    }
+    public int cikarma() {
+        return this.sayi1 - this.sayi2;
+    }
+    public int carpma() {
+        return this.sayi1 * this.sayi2;
+    }
+    public int bolme() {
+        return this.sayi1 / this.sayi2;
+    }
+}
